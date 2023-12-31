ec1a9552cd119f9844f96d7af1d80b5f46ee89cf
Ravindu Chamod Wickramage <ravinduchamod1@gmail.com>
2023-12-31 23:37:50 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+import { TypeAnimation } from "react-type-animation";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            <TypeAnimation
+              sequence={[
+                'Your Favourite Food Delivered  Fresh',
+                1000,
+                'Your Favourite Food Delivered  Hot',
+                1000,
+                'Your Favourite Food Delivered Delicious',
+                1000,
+              ]}
+              speed={50}
+              style={{ fontSize: '1em' }}
+              repeat={Infinity}
+            />
+            </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep work, like peeding, chopping
+            & marinating, so you can cook a fresh food.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
